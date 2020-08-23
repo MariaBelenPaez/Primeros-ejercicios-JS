c935b389ad2da87dc3592f1cbf9246e36cb4dbc9
María <belenpaez@live.com.ar>
2020-08-22 23:40:41 -0300

    cambiosejerciciosminutos

--- index.js
+++ index.js
@@ -1,61 +1,67 @@
-let nombredeusuaria = prompt("Decime tu nombre")
-let apellidodeusuaria = prompt("Decime tu apellido")
+// let nombredeusuaria = prompt("Decime tu nombre")
+// let apellidodeusuaria = prompt("Decime tu apellido")
 
-alert("Hola," + nombredeusuaria + " " + apellidodeusuaria + ", Bienvenida a Ada! ")
+// alert("Hola," + nombredeusuaria + " " + apellidodeusuaria + ", Bienvenida a Ada! ")
 
-let primerGusto = prompt("Indique el primer gusto de helado")
-let segundoGusto = prompt("Indique el segundo gusto de helado")
-let tercerGusto = prompt("Indique el tercer gusto de helado")
+// let primerGusto = prompt("Indique el primer gusto de helado")
+// let segundoGusto = prompt("Indique el segundo gusto de helado")
+// let tercerGusto = prompt("Indique el tercer gusto de helado")
 
-alert(`Aqui tiene su helado de ${primerGusto},${segundoGusto},${tercerGusto}`)
+// alert(`Aqui tiene su helado de ${primerGusto},${segundoGusto},${tercerGusto}`)
 
-Crear un programa que pida al usuario ingresar, por separado, nombre, apellido, edad, nacionalidad, documento,
-y muestre luego un mensaje que diga: "Nuevo usuario agregado al sistema:"
-y liste todos los datos juntos.
+// Crear un programa que pida al usuario ingresar, por separado, nombre, apellido, edad, nacionalidad, documento,
+// y muestre luego un mensaje que diga: "Nuevo usuario agregado al sistema:"
+// y liste todos los datos juntos.
 
-let nombre = prompt("Indique su nombre")
-let apellido = prompt("Indique su apellido")
-let edad = prompt("Indique su edad")
-let nacionalidad = prompt("Indique su nacionalidad")
-let documento = prompt("Indique su numero de documento")
+// let nombre = prompt("Indique su nombre")
+// let apellido = prompt("Indique su apellido")
+// let edad = prompt("Indique su edad")
+// let nacionalidad = prompt("Indique su nacionalidad")
+// let documento = prompt("Indique su numero de documento")
 
-alert(`NUEVO USUARIO INGRESADO\n ${nombre},${apellido},${edad},${nacionalidad},${documento}`)
+// alert(`NUEVO USUARIO INGRESADO\n ${nombre},${apellido},${edad},${nacionalidad},${documento}`)
 
-Lista de reproducción
+// Lista de reproducción
 
-Crear un programa que pida al usuario ingresar, por separado, el nombre de una playlist
-y el título de tres canciones, y al finalizar se muestre un mensaje que diga, p.ej.:
-    "Se ha creado la playlist 'Hits de los 80s' con las canciones 'Africa', 'Maniac', 'Final Countdown'".
+// Crear un programa que pida al usuario ingresar, por separado, el nombre de una playlist
+// y el título de tres canciones, y al finalizar se muestre un mensaje que diga, p.ej.:
+//     "Se ha creado la playlist 'Hits de los 80s' con las canciones 'Africa', 'Maniac', 'Final Countdown'".
 
-let nombrePlaylist = prompt("Como se llamara esta lista?")
-let primerCancion = prompt("Agrega tu primer cancion")
-let segundaCancion = prompt("Excelente eleccion, agrega otra!")
-let tercerCancion = prompt("Completa tu lista agregando mas")
+// let nombrePlaylist = prompt("Como se llamara esta lista?")
+// let primerCancion = prompt("Agrega tu primer cancion")
+// let segundaCancion = prompt("Excelente eleccion, agrega otra!")
+// let tercerCancion = prompt("Completa tu lista agregando mas")
 
-alert(`Se ha creado la playlist\n ${nombrePlaylist}\n ${primerCancion}\n ${segundaCancion}\n ${tercerCancion}`)
+// alert(`Se ha creado la playlist\n ${nombrePlaylist}\n ${primerCancion}\n ${segundaCancion}\n ${tercerCancion}`)
 
-Dirección completa
+// Dirección completa
 
-Crear un programa que pida al usuario ingresar, por separado, la calle, el número, el departamento, el código postal, la ciudad y el país, y muestre un mensaje con toda la dirección completa,
+// Crear un programa que pida al usuario ingresar, por separado, la calle, el número, el departamento, el código postal, la ciudad y el país, y muestre un mensaje con toda la dirección completa,
 
-p.ej.: "La dirección que ha ingresado es: Calle Falsa 123 3C, 1414, CABA, Argentina".
+// p.ej.: "La dirección que ha ingresado es: Calle Falsa 123 3C, 1414, CABA, Argentina".
 
-let calle = prompt("Indique la calle de su domicilio?")
-let numero = prompt("Indique el numero de su domicilio?")
-let departamento = prompt("Indique departamento")
-let codigoPostal = prompt("Indique su codigo postal")
-let ciudad = prompt("Indique ciudad")
+// let calle = prompt("Indique la calle de su domicilio?")
+// let numero = prompt("Indique el numero de su domicilio?")
+// let departamento = prompt("Indique departamento")
+// let codigoPostal = prompt("Indique su codigo postal")
+// let ciudad = prompt("Indique ciudad")
 
-alert(`La direccion que usted ha ingresado es:${calle},${numero},${departamento},${codigoPostal},${ciudad}`)
+// alert(`La direccion que usted ha ingresado es:${calle},${numero},${departamento},${codigoPostal},${ciudad}`)
 
-Edad de perros
-let edadHumano = prompt("Cual es tu edad?")
+// Edad de perros
+// let edadHumano = prompt("Cual es tu edad?")
 
 
-alert(`Tu edad perruna es ${edadHumano*7}`)
+// alert(`Tu edad perruna es ${edadHumano*7}`)
 
-Minutos a Segundos
+// Minutos a Segundos
 
-let minutos = prompt("Ingrese minutos")
+// let minutos = prompt("Ingrese minutos")
 
-alert(`Sus minutos son ${minutos*60} en segundos`)
+// alert(`Sus minutos son ${minutos*60} en segundos`)
+
+//Triangulo 
+let base = prompt("Indique el valor de la base")
+let altura = prompt("Indique el valor de la altura")
+
+alert(`El area equivale a ${base*altura/2} `)
